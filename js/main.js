790b5264a68f811716025c64f87846d0819e0911
khanh <khanhhoang197.wbs@gmail.com>
2023-02-01 22:11:32 +0700

    my portfolio

--- js/main.js
+++ js/main.js
@@ -57,6 +57,14 @@
 
 
     // Portfolio isotope and filter
+   
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
     var portfolioIsotope = $('.portfolio-container').isotope({
         itemSelector: '.portfolio-item',
         layoutMode: 'fitRows'
@@ -67,6 +75,7 @@
 
         portfolioIsotope.isotope({filter: $(this).data('filter')});
     });
+
 
 
     // Testimonials carousel
