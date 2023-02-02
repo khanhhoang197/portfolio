cedd16d71e7775e412e0bc8049d1b98620450b35
khanh <khanhhoang197.wbs@gmail.com>
2023-02-02 11:34:48 +0700

    my portfolio

--- js/main.js
+++ js/main.js
@@ -57,13 +57,6 @@
 
 
     // Portfolio isotope and filter
-   
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
-
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
 
     var portfolioIsotope = $('.portfolio-container').isotope({
         itemSelector: '.portfolio-item',
